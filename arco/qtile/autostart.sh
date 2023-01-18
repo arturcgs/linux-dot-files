c9e0075b81b388a5311a95ba5fa3da45ba86ecb0
Artur Caminero Gomes Soares <arturcaminero@gmail.com>
2023-01-18 09:21:22 -0300

    testing script

--- arco/qtile/autostart.sh
+++ arco/qtile/autostart.sh
@@ -7,7 +7,7 @@ function run {
   fi
 }
 
-# set 2nd screen resolution
+# set 2nd screen resolutionn
 run xrandr --output eDP-1-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output DP-1-1 --off
 
 # set wallpaper
