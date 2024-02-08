4a1b273c29a56132dfbf3bbb115c6caedc6a0f58
Artur C <arturcaminero@gmail.com>
2024-02-08 10:22:01 -0300

    update dunstrc and pomodoro script

--- qtile_ubuntu/scripts/general/pomodoro.sh
+++ qtile_ubuntu/scripts/general/pomodoro.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-# Sleep for 25 minutes
-sleep 25m
+# get current time 
+current_time=`date +"%H:%M"`
+final_time=`date -d '+25 minutes' '+%H:%M'`
 
-# Send a notification to both screens
+# Messages
+echo "Iniciando Pomodoro de 25 minutos"
+echo "Início: $current_time"
+echo "Final : $final_time"
+sleep 5m
+echo "Faltam 20 minutos"
+
+sleep 5m
+echo "Faltam 15 minutos"
+
+sleep 5m
+echo "Faltam 10 minutos"
+
+sleep 5m
+echo "Faltam 5 minutos"
+
+sleep 5m
+
+# Send a notification
+echo "Final da Sessão de Trabalho!"
 notify-send "Aviso" "Final da Sessão de trabalho!"
 
-# Play a sound (adjust the path to your sound file)
+# Play a sound
 paplay /home/artur/Documents/sounds/alarm-clock.wav
 
 # Sleep for 6 minutes
 sleep  6m
 
-# Send a notification to both screens
+# Send a notification
+echo "Final da pausa!"
 notify-send "Aviso" "Final da pausa!"
 
-# Play a sound (adjust the path to your sound file)
+# Play a sound
 paplay /home/artur/Documents/sounds/alarm-clock.wav
